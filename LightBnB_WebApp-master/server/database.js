cbfa563eb335985b73c847ae6cbdc8ec18a4e15a
Karolina Redden <dubajkaro@gmail.com>
2022-04-19 00:55:41 +0000

    getAllProperties function refactored with city filter

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -108,17 +108,84 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
-const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+// const getAllProperties = (options, limit = 10) => {
+  // return pool
+  //   //.query(`SELECT * FROM properties LIMIT $1`, [limit])
+
+  //   .query(`SELECT properties.*,
+  //   avg(property_reviews.rating) as average_rating
+  //   FROM properties
+  //   LEFT JOIN property_reviews ON properties.id = property_id
+  //   WHERE city LIKE '%ancouv%'
+  //   GROUP BY properties.id
+  //   HAVING avg(property_reviews.rating) >= 4
+  //   ORDER BY cost_per_night
+  //   LIMIT 10;`)
+
+    // .then((result) => {
+    //   return result.rows;
+    // })
+    // .catch((err) => {
+    //   console.log(err.message);
+    // });
+    // const queryParams = [];
+    // //Start the query with all information that comes before the WHERE clause.
+    // let queryString = `
+    // SELECT properties.*, avg(property_reviews.rating) as average_rating
+    // FROM properties
+    // JOIN property_reviews ON properties.id = property_id
+    // `;
+//Check if a city has been passed in as an option. Add the city to the params array and create a WHERE clause for the city.
+    // if (options.city) {
+    //   queryParams.push(`%${options.city}%`);
+    //   queryString += `WHERE city LIKE $${queryParams.length} `;
+    // }
+
+//     queryParams.push(limit);
+//     queryString += `
+//     GROUP BY properties.id
+//     ORDER BY cost_per_night
+//     LIMIT $${queryParams.length};
+//     `;
+
+//     console.log(queryString, queryParams);
+//     return pool.query(queryString, queryParams).then((res) => res.rows);
+// };
+
+
+const getAllProperties = function (options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};`;
+  // 5
+  console.log(queryString, queryParams);
+  // 6
+  return pool.query(queryString, queryParams).then((res) => {
+    console.log(res.rows)
+    return res.rows
+  });
 };
-getAllProperties();
+
+
+getAllProperties({city: 'Vancouver'});
 
 
 exports.getAllProperties = getAllProperties;
